ab8bf12c5ce938d017d28cebacb37367b70fb837
Adeniyi-Bella <adeniyibella@gmail.com>
2023-08-26 01:31:53 +0200

    feat: added boiler template

---
+++ apisecurity/src/test/java/io/javatech/apisecurity/ApisecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javatech.apisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
